0eeb5112d64ee5382bb22080942ef3a20e6da489
kabui100 <andybuibui2020@gmail.com>
2025-10-28 21:41:56 +0000

    cheese 2

--- Main.java
+++ Main.java
@@ -5,6 +5,16 @@ public class Main
   public static void main(String[] args)
   {
     printNTimes("hello", 5); 
+
+    Scanner sc = new Scanner(System.in);
+    int num; 
+    System.out.println("How many rows of numbers do you want?");
+    num = sc.nextInt(); 
+    uprightNumberTriangle(num); 
+
+    int number;
+    System.out.println("Input a number"); 
+    number = sc.nextInt(); 
   }
 
   public static void printNTimes(String word, int N)
@@ -23,12 +33,23 @@ public class Main
 
   public static void printNums()
   {
-    
+    for (int rowNum = 10; rowNum >= 1; rowNum--)
+    {
+      for (int count = 1; count <= rowNum; count++)
+      {
+        System.out.print(rowNum + " ");
+      }
+      System.out.println();
+    }
   }
 
   public static void uprightNumberTriangle(int N)
   {
-    // code solution here
+    for (int rowNum = 1; row <= N; rowNum++)
+    {
+      for (int current = 1; current <= row )
+    }
+    
   }
 
   public static void starTree()
